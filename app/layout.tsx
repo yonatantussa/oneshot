2a4b25d37719ee74022c6dbb6e7ab1cbd5a26e32
Yonatan Tussa <140031110+yonatantussa@users.noreply.github.com>
2025-10-15 06:30:54 -0400

    add favicon and extend background to bottom on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'oneshot',
   description: 'understand anything',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
